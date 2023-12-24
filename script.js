14838b658a844a10c45fd7de97085e3d15a43f49
Hanson50 <hansonedidiong@yahoo.com>
2023-12-24 06:39:23 +0100

    My first project at altschool

---
+++ script.js
@@ -0,0 +1,251 @@
+// console .log("Helo");
+
+// let Company = prompt("What is the Name of your School");
+// if (Company == "NNS QUORRA"){
+//     alert("You'r right")
+// } else{
+//     alert("Go and read")
+// };
+
+// let Subject = ["Chemistry", "Maths", "Biology", "Agric", "computer", "Physics", "Economics"]
+// alert (Subject[2]);
+// console.log(Subject.length);
+// console.log(Subject["0"]);
+// Subject [3] = "Goat"
+// Subject [7] = "fish"
+
+// console.log(Subject);
+// Subject .push ("Hanson", "Edidiong", "Akpan");
+// Subject.pop()
+// Subject.pop()
+// Subject.pop()
+// Subject.pop()
+// console.log(Subject);
+
+// let Isenrolled = false;
+// if(Isenrolled) {
+//     console.log("Take the class");
+// }
+
+// else{
+//     console.log("Enrolled first")
+// }
+
+// let fName = "Edidiong";
+// lName = "Hanson";
+// console.log (fName, lName);
+
+// const age = 25;
+// const size = 40;
+
+// console.log(age, size);
+
+// let fullname = " Godknow Okon Hanson";
+// console.log("oldname"  + fullname);
+
+// const fname = " Akpan Mike";
+// console.log ("New name"  + fname);;
+
+// //Creating An object in Js
+// let user1 ={}
+// user1 ["name"] = "Hanson";
+// user1["age"] = 20;
+// user1 ["username"] = "Hanson@50";
+// user1 ["email-verified"] = true;
+// console.log(user1)
+ 
+
+// let userDetail = {}
+// userDetail["Name"] = "Edidiong";
+// userDetail["Age"] = 25;
+// userDetail["username"] ="Hanson@50";
+// userDetail["email-verified"] = "true";
+// console.log (userDetail)
+
+// let userDetail = {}
+// userDetail["name"] = "Edidiong";
+// userDetail ["age"] = "25yrs"
+// userDetail["email-verified"] = "hansonedidiong@yahoo.com";
+// userDetail ["username"] = "Hanson@50";
+// userDetail ["phone no:"]    = "08029585837";
+// userDetail["Address"] = "NNS Quorra"
+// console.log(userDetail);
+
+// console.log(userDetail.name);
+// console.log (userDetail.age);
+// userDetail["age"]= 35;
+// console.log(userDetail);
+// delete userDetail.age
+
+// console.log(userDetail);
+// userDetail["name"] = "Hanson"
+// console.log(userDetail)
+// console.log(Object.values(userDetail))
+
+// let Company = prompt("NNS QUORRA Lagos");
+//     if (Company == "NNS QUORRA") {
+//         console.log("You're For real")   
+// }
+// else{
+//     console.log("Check it again")
+// };
+
+// let FirstName = "Edidiong"
+// LastName = " Hanson"
+
+// alert(`Mr ${FirstName}${LastName}`);
+
+// let greeting = "Welcome to 2024 as successful \n";
+// checkout = "try and get marry in 2024 young man"
+// console.log(`Mr Hanson ${greeting} ${checkout}`);
+
+// let TheCompany = prompt("What is the official Name of Javascript");
+// if(TheCompany == "ECMAscript"){
+//     console.log("Right");
+// }else{ console.log
+//     ("Wrong again")
+// }
+
+// let massage = "Edidiong_Okon_Hanson\n my status ig growing everything day of my life\n software developer";
+// console.log(massage.toUpperCase())
+// console.log(massage.startsWith("cat"));
+
+// let day = "Friday"
+// if(day == "Friday"){
+//     console.log("Today is Friday")
+//     alert("You're successful enter Friday");
+// }
+
+//     let toDay = "Monday"
+//     if(toDay == "Monday"){
+//         console.log("Todays is Monday")
+//     }
+//    else if (toDay == "Tuesday"){
+//     console.log("Today is Tueday")
+//    }
+//    if (toDay == "Wednesday"){
+//     console.log("Today is Wedneday")
+//    }
+//    else if (toDay == "Thurday"){
+//     console.log("Today is Wednesday")
+//    }
+//    if(toDay == "Friday"){
+//     console.log("Today is Friday")
+//    }
+//    else{
+//     console.log("Can not print")
+//    }
+
+// let message = "Friday";
+// if(message == "Friday")
+// {
+//     console.log("Wonderful day")
+// }
+
+let today = "onday";
+if(today == "Monday"){
+    console.log("Today is Monday")
+}else if (today == "Tuesday")
+{
+    console.log("Today is Tuesday")
+}
+else if (today == "Wednesday")
+{
+    console.log("Today is wednesday")
+} else{
+    console.log("welcome to the month of free drink")
+}
+ let Age = 25;
+ if(Age == 25){
+    console.log("can vote")
+ }else {
+    console.log("can not Vote")
+ };
+
+ let age = 16;
+ let canvote = (age==16)? true : false;
+ console.log(canvote);
+
+ function toCelsius(f) {
+    return (5/9) * (f-32);
+ }
+
+
+
+let Subject = {}
+Subject["name"] = "Edidiong";
+Subject["age"] = 25;
+Subject["email-verified"] = "true";
+Subject["uersname"] = "hanson@50";
+console.log(Subject);
+delete Subject.name
+console.log(Subject);
+console.log(Subject.age);
+Subject ["age"] = 69
+console.log(Subject);
+
+
+
+// let Object = {
+//     name: "Edidong",
+//     age: 50,
+//     phone: "08029585837",
+//     location: "lagos",
+//     status: "Male",
+
+//     Object: function()
+// {
+//   return this. name + ""+ this.age
+// }
+
+// }
+
+console.log(Object)
+delete Object. name;
+console.log(Object)
+console.log(Object.age)
+
+console.log(Object.phone  + " " + Object.location);
+
+
+var subject = "This is my first time to travelling";
+// let position = subject.indexOf("first")
+
+
+var txt = subject.replace("my", "50");
+console.log(txt);
+
+let word = "HELLO WORD";
+console.log(word[1]);
+
+let Cars = ["BMW", "VOLVE", "TOYOTA", "MATRIX", "PATHFINDER", "ACCORD"]
+console.log(Cars.length);
+console.log(Cars[2]);
+console.log(typeof Cars)
+
+Cars.sort()
+console.log(Cars)
+
+// let userValidation  = {}
+// userValidation["name"] = "Edidiong";
+// userValidation ["age"] = "25yr"
+// userValidation ["location"] = "Lagos"
+// userValidation ["username"] = "Hanson@50";
+// console.log(userValidation);
+// userValidation["name"] = "Hanson"
+// console.log(userValidation["name"])
+// console.log(userValidation)
+// console.log(typeof userValidation)
+
+let userValidation = {
+    name: "Hanson",
+    age : "25yrs",
+    email: "hansonedidiong@yahoo.com",
+    phone: "08029585837"
+}
+console.log(userValidation)
+let fruit = ["Apple", "Banana", "Orang", "Grape"]
+console.log(fruit["0"]);
+first = fruit [0];
+console.log(first)
+console.log
